aaa2e31b8f4f037e6cfa7bb195535bc6d8d5b698
Hassani <hassanisnr@gmail.com>
2019-02-02 15:05:58 -0500

    Added user to Dockerfile & recreated project.

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nygqx9jt74j_ap@k+6@s#!1%8ew+gtuest@2kdlk+5(ft1r@q8'
+SECRET_KEY = 'psvz1=gkhms*l@@%6@&io5fym40i@=j2xvh-!21tvc283j96#&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
