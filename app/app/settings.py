0b2741ec7caa6469ed88dbeedf6dc5bcc71a5402
Hassani <hassanisnr@gmail.com>
2019-04-28 09:13:39 -0400

    Adding procfile and modified media & static roots

--- app/app/settings.py
+++ app/app/settings.py
@@ -102,8 +102,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = '/vol/web/media'
-STATIC_ROOT = '/vol/web/static'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 AUTH_USER_MODEL = 'core.User'
 
