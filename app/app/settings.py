efadc91fe537465e58ac980ccf1da786706aff9b
Hassani <hassanisnr@gmail.com>
2019-04-27 20:01:04 -0400

    Removing swagger from project

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles', 
     'rest_framework',
     'rest_framework.authtoken',
-    'rest_framework_swagger',
     'core',
     'user',
     'recipe',
--- app/app/urls.py
+++ app/app/urls.py
@@ -2,13 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from rest_framework_swagger.views import get_swagger_view
 
-schema_view = get_swagger_view(title="Recipe API")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('user.urls')),
     path('api/recipe/', include('recipe.urls')),
-    path('api/swagger', schema_view)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
